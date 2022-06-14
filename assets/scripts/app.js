5a20f4d8bc60a3b745a6cc3d64989feb41a5c41e
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-14 22:09:32 +0300

    feat: each project list parses through its list items to find out which projects belong to them

---
+++ assets/scripts/app.js
@@ -0,0 +1,19 @@
+class ToolTip {}
+
+class ProjectItem {}
+
+class ProjectList {
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    console.log(prjItems)
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+  }
+}
+
+App.init();
